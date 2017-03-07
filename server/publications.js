590bcbd89050db4ecb24167c8cb3c13fa08b211b
Sam Hamra <samhamra@Sams-MBP.lan>
2017-03-08 00:12:03 +0100

    s

--- server/publications.js
+++ server/publications.js
@@ -7,6 +7,19 @@ Meteor.publish('topic', function(currentTopic) {
 Meteor.publish('onlineUsers', function() {
   return Meteor.users.find({"status.online": true})
 })
-Meteor.publish('mainChatRoom', function() {
-  return ChatRooms.find({room: "main"});
+Meteor.publish('mainChatRoom', function(date) {
+  console.log(date)
+  var hej = ChatRooms.aggregate([
+    { $match: {'room': 'main'}},
+    { $project: {
+        list: {$filter: {
+            input: '$messages',
+            as: 'item',
+            cond: {$gt: ['$$item.message.createdAt', date]}
+        }}
+    }}
+])
+  console.log(hej);
+
+  return hej;
 })
